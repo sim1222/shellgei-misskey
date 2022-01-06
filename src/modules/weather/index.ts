74cf3299a72bed428363fef366a6db9f6b94cca8
sim1222 <kokt@sim1222.com>
2022-01-07 00:29:21 +0900

    うぃｐ

--- src/modules/weather/index.ts
+++ src/modules/weather/index.ts
@@ -2,6 +2,8 @@ import autobind from 'autobind-decorator';
 import Module from '@/module';
 import Message from '@/message';
 import serifs from '@/serifs';
+import config from '@/config';
+import json from '@/json';
 
 export default class extends Module {
 	public readonly name = '';
@@ -15,26 +17,14 @@ export default class extends Module {
 
 	@autobind
 	private async mentionHook(msg: Message) {
-		if (msg.text == null) return false;
-
-		const query = msg.text.match(/([0-9]+)[dD]([0-9]+)/);
-
-		if (query == null) return false;
-
-		const times = parseInt(query[1], 10);
-		const dice = parseInt(query[2], 10);
-
-		if (times < 1 || times > 10) return false;
-		if (dice < 2 || dice > 1000) return false;
-
-		const results: number[] = [];
-
-		for (let i = 0; i < times; i++) {
-			results.push(Math.floor(Math.random() * dice) + 1);
+		if (msg.text && msg.text.includes('天気') && msg.text.includes('天気')) {
+			
+			msg.reply('PONG!', {
+				immediate: true
+			});
+			return true;
+		} else {
+			return false;
 		}
-
-		msg.reply(serifs.dice.done(results.join(' ')));
-
-		return true;
 	}
 }
