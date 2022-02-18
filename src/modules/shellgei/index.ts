92411d3f92c77ad57e9632f2b2ab1e74c7683c55
sim1222 <kokt@sim1222.com>
2022-02-19 05:03:17 +0900

    fix: test

---
+++ src/modules/shellgei/index.ts
@@ -0,0 +1,28 @@
+import autobind from 'autobind-decorator';
+import Module from '@/module';
+import Message from '@/message';
+
+export default class extends Module {
+	public readonly name = 'shellgei';
+
+	@autobind
+	public install() {
+		return {
+			mentionHook: this.mentionHook
+		};
+	}
+
+	@autobind
+	private async mentionHook(msg: Message) {
+		if (!msg.text) return false;
+		if (msg.text && msg.text.includes('#シェル芸') || msg.text.includes('#shellgei')) {
+			const shelltext = msg.text.replace('#シェル芸', '').replace('#shellgei', '');
+			msg.reply('PONG!' + shelltext, {
+				immediate: true
+			});
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
