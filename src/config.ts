4e18b745f019f47ef25af7c658703116a50c9269
sim1222 <kokt@sim1222.com>
2022-02-19 04:46:24 +0900

    fix: おそうじ

--- src/config.ts
+++ src/config.ts
@@ -4,13 +4,6 @@ type Config = {
 	master?: string;
 	wsUrl: string;
 	apiUrl: string;
-	keywordEnabled: boolean;
-	reversiEnabled: boolean;
-	notingEnabled: boolean;
-	chartEnabled: boolean;
-	serverMonitoring: boolean;
-	mecab?: string;
-	mecabDic?: string;
 	memoryDir?: string;
 };
 
--- src/index.ts
+++ src/index.ts
@@ -12,28 +12,8 @@ import _log from './utils/log';
 const pkg = require('../package.json');
 
 import CoreModule from './modules/core';
-import TalkModule from './modules/talk';
-import BirthdayModule from './modules/birthday';
-import ReversiModule from './modules/reversi';
 import PingModule from './modules/ping';
-import EmojiModule from './modules/emoji';
-import EmojiReactModule from './modules/emoji-react';
-import FortuneModule from './modules/fortune';
-import GuessingGameModule from './modules/guessing-game';
-import KazutoriModule from './modules/kazutori';
-import KeywordModule from './modules/keyword';
-import WelcomeModule from './modules/welcome';
-import TimerModule from './modules/timer';
-import DiceModule from './modules/dice';
-import ServerModule from './modules/server';
 import FollowModule from './modules/follow';
-import ValentineModule from './modules/valentine';
-import MazeModule from './modules/maze';
-import ChartModule from './modules/chart';
-import SleepReportModule from './modules/sleep-report';
-import NotingModule from './modules/noting';
-import PollModule from './modules/poll';
-import ReminderModule from './modules/reminder';
 
 console.log('   __    ____  _____  ___ ');
 console.log('  /__\\  (_  _)(  _  )/ __)');
@@ -66,28 +46,9 @@ promiseRetry(retry => {
 	// 藍起動
 	new 藍(account, [
 		new CoreModule(),
-		new EmojiModule(),
-		new EmojiReactModule(),
-		new FortuneModule(),
-		new GuessingGameModule(),
-		new KazutoriModule(),
-		new ReversiModule(),
-		new TimerModule(),
-		new DiceModule(),
-		new TalkModule(),
 		new PingModule(),
-		new WelcomeModule(),
-		new ServerModule(),
 		new FollowModule(),
-		new BirthdayModule(),
-		new ValentineModule(),
-		new KeywordModule(),
-		new MazeModule(),
-		new ChartModule(),
-		new SleepReportModule(),
-		new NotingModule(),
-		new PollModule(),
-		new ReminderModule(),
+
 	]);
 }).catch(e => {
 	log(chalk.red('Failed to fetch the account'));
